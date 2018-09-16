45bfa5cf8c17aa4ac2d6949e5332a3d7950d3ce9
Short Stack <41342213+rchlblns@users.noreply.github.com>
2018-09-16 13:04:51 -0500

    first question displays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,86 +1,174 @@
-//Global variables
-var totalCorrect = 0;
-var totalWrong = 0;
+$(document).ready(function(){
 
-var questions = [
-    {
-        question: "What is Neville Longbottom's bogart?", 
-        answerChoices: ["A spider", "Professor Snape", "His grandma", "A dementor"],
-        correctAnswer: "Professor Snape",
-    },
-    {
-        question: "Where does Harry Potter live?", 
-        answerChoices: ["Privet Drive", "Ottery Street", "Grimmauld Place", "Little Hangleton"],
-        correctAnswer: "Privet Drive",
-    },
-    {
-        question: "In addition to teaching potions, Professor Snape also taught what other subject?", 
-        answerChoices: ["Transfiguration", "Charms", "Defense Against the Dark Arts", "Divination"],
-        correctAnswer: "Defense Against the Dark Arts",
-    },
-    {
-        question: "Who drives the Knight Bus?", 
-        answerChoices: ["Stan Shunpike", "Marcus Belby", "Ernie Prang", "Reginald Cattermore"],
-        correctAnswer: "Stan Shunpike",
-    },
-    {
-        question: "Which two countries competed in the Quidditch World Cup?", 
-        answerChoices: ["England and France", "Egypt and Moldova", "Senegal and China", "Ireland and Bulgaria"],
-        correctAnswer: "Ireland and Bulgaria",
-    },
-    {
-        question: "How many Weasley siblings are there?", 
-        answerChoices: ["5", "6", "7", "8"],
-        correctAnswer: "7",
-    },
-    {
-        question: "What article of clothing makes Dobby a free elf?", 
-        answerChoices: ["A shoe", "A sock", "A hat", "A shirt"],
-        correctAnswer: "A sock",
-    },
-    {
-        question: "What is Dumbledore's favorite candy?", 
-        answerChoices: ["Lemon Drops", "Exploding Bonbons", "Chocolate Cauldrons", "Fizzing Wizzbees"],
-        correctAnswer: "Lemon Drops",
-    },
-    {
-        question: "Whhich platfrom is the Hogwarts Express located at?", 
-        answerChoices: ["Four and three-eights", "Twelve and one-half", "Five and one-thirds", "Nine and three-quarters"],
-        correctAnswer: "Nine and three-quarters",
-    },
-    {
-        question: "Which spell is used to make objects fly?", 
-        answerChoices: ["Obliviate", "Alohamora", "Wingardium Leviosa", "Lumos"],
-        correctAnswer: "Wingardium Leviosa",   
+    var timeLeft;
+    var userSelect;
+    var answered;
+    var correctAnswer = 0; //total times user guessed right
+    var incorrectAnswer= 0; // total times user guessed wrong
+    var noGuess = 0; //total times user did not guess answer
+    var currentQuestionIndex = 0; //counts how many questions have been asked
+
+    var questionArray = [
+        {
+            question: "What is Neville Longbottom's bogart?", 
+            choices: ["A spider", "Professor Snape", "His grandma", "A dementor"],
+            correct: 1,
+        },
+        {
+            question: "Where does Harry Potter live?", 
+            choices: ["Privet Drive", "Ottery Street", "Grimmauld Place", "Little Hangleton"],
+            correct: 0,
+        },
+        {
+            question: "In addition to potions, Professor Snape also taught what other subject?", 
+            choices: ["Transfiguration", "Charms", "Defense Against the Dark Arts", "Divination"],
+            correct: 2,
+        },
+        {
+            question: "Who is the conductor of the Knight Bus?", 
+            choices: ["Stan Shunpike", "Marcus Belby", "Ernie Prang", "Reginald Cattermore"],
+            correct: 0,
+        },
+        {
+            question: "Which two countries competed in the Quidditch World Cup?", 
+            choices: ["England and France", "Egypt and Moldova", "Senegal and China", "Ireland and Bulgaria"],
+            correct: 3,
+        },
+        {
+            question: "How many Weasley siblings are there?", 
+            choices: ["5", "6", "7", "8"],
+            correct: 2,
+        },
+        {
+            question: "What article of clothing makes Dobby a free elf?", 
+            choices: ["A shoe", "A sock", "A hat", "A shirt"],
+            correct: 1,
+        },
+        {
+            question: "What is Dumbledore's favorite candy?", 
+            choices: ["Lemon Drops", "Exploding Bonbons", "Chocolate Cauldrons", "Fizzing Wizzbees"],
+            correct: 0,
+        },
+        {
+            question: "Whhich platfrom is the Hogwarts Express located at?", 
+            choices: ["Four and three-eights", "Twelve and one-half", "Five and one-thirds", "Nine and three-quarters"],
+            correct: 3,
+        },
+        {
+            question: "Which spell is used to make objects fly?", 
+            choices: ["Obliviate", "Alohamora", "Wingardium Leviosa", "Lumos"],
+            correct: 2,   
+        }
+    ];
+
+    var gifArray = ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10"];
+
+    var messages = {
+        correct: "Nice! You got it right!",
+        incorrect: "Nope!",
+        outOfTime: "Times up!",
+        done: "Alright! Let's see how you did."
     }
-]
 
-//Sets number counter to 60
-var number = 60;
-//Variable that holds interval ID when we execute the "run" function
-var intervalId;
+    $("#startBtn").on("click", function(){
+        $(this).hide();
+        newGame();
+    });
 
-//function to run the decrement function once a second
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-}
-
-//Function to decrement the number
-function decrement() {
-    number--;
-    $("#timer").html("<h2>" + number + "</h2>");
-    if (number === 0) {
-        stop();
-        console.log("Times up!");
+    function newGame() {
+        $("#finalMessage").empty();
+        $("#correctAnswers").empty();
+        $("#wrongAnswers").empty();
+        $("#noAnswer").empty();
+        currentQuestionIndex = 0;
+        correctAnswer = 0;
+        incorrectAnswer = 0;
+        noGuess = 0;
+        newQuestion();
+        // countdown();
     }
-}
 
-console.log(number);
+    function newQuestion() {
 
-//Stop function
-function stop() {
-    clearInterval(intervalId);
-}
+        $("#message").empty();
+        $("#answerReveal").empty();
+        $("#gif").empty();
+        answered = true;
+        countdown();
+        
+        $("#currentQuestion").html("<h2>" + questionArray[currentQuestionIndex].question + "</h2>");
 
-run();
+        for (var i =0; i < 4; i++) {
+            var choiceList = $("<div>");
+            choiceList.text(questionArray[currentQuestionIndex].choices[i]);
+            choiceList.attr("data-index", i);
+            choiceList.addClass("thisChoice");
+            $("#answerChoices").append(choiceList);
+        }
+
+        //on click event to track the user's answer selection
+        $("#thisChoice").on("click", function(){
+            userSelection = $(this).data('index');
+            clearInterval(time);
+            answerPage();
+        });
+    }
+
+    function countdown() {
+        var timeLeft = 60;
+        $("#timer").html("<h3> Time remaining: " + timeLeft + "</h3>");
+        answered = true;
+        time = setInterval(showCountdown, 1000);
+    }
+
+    function showCountdown() {
+        timeLeft--;
+        $("#timer").html("<h3> Time remaining: " + timeLeft + "</h3>");
+        if (timeLeft === 0) {
+            clearInterval(time);
+            answered = false;
+            answerPage();
+        }
+    }
+
+    function answerPage() {
+        $(".currentQuestion").empty();
+        $(".thisChoice").empty();
+
+    //     //this variable is equal to the answer of the current question
+    //     var rightAnswerText = questionArray[currentQuestionIndex].answerList[questionArray[currentQuestionIndex].answer];
+    //     var rightAnswerIndex = questionArray[currentQuestionIndex].answer;
+    //     $('#gif').html('<img src = "assets/images/'+ gifArray[currentQuestionIndex] +'.gif" width = "400px">');
+
+    //     //checks to see if answer was correct or incorrect or if unanswered
+    //     if ((userSelection == rightAnswerIndex) && (answered == true)){
+    //         correctAnswer++;
+    //         $('#message').html(messages.correct);
+    //     } 
+    //     else if ((userSelection != rightAnswerIndex) && (answered == true)){
+    //         incorrecttAnswer++;
+    //         $('#message').html(messages.incorrect);
+    //         $('#answerReveal').html('The correct answer was: ' + rightAnswerText);
+    //     } 
+    //     else { 
+    //         noAnswer++;
+    //         $('#message').html(messages.endTime);
+    //         $('#answerReveal').html('The correct answer was: ' + rightAnswerText);
+    //         answered = true;
+    //     }
+        
+    //     if (currentQuestionIndex == (questionArray.length-1)){
+    //         setTimeout(scoreboard, 5000)
+    //     } 
+    //     else {
+    //         currentQuestion++;
+    //         setTimeout(newQuestion, 5000);
+    //     }	
+    }
+
+    $("#startOverBtn").on("click", function(){
+        $(this).hide();
+        newGame();
+    });
+    
+});
